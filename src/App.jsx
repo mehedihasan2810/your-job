422a54f5b42bea629091955aefc2787cec6949aa
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-04-10 12:08:45 +0600

    initial and project setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar/Navbar';
+
+const Layout = () => (
+  <div>
+    <Navbar/>
+    
+  </div>
+)
+
+const App = () => {
+  return (
+    
+    <>
+    <div>App</div>
+    <img src="./assets/Icons/accounts 1.png" alt="" />
+    </>
+
+  )
+}
+
+export default App
